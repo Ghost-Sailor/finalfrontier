1c7aea05484ef52ee243ada6c27f945a9e571f92
Ghost-Sailor <ghostsailor@gmx.us>
2013-10-14 17:46:37 -0700

    Update sh_teams.lua

--- gamemode/sh_teams.lua
+++ gamemode/sh_teams.lua
@@ -19,63 +19,56 @@ function TableShuffle(t)
 end
 
 if SERVER then
-function GetMapSetPos()
-if game.GetMap() == ("finalfrontier_a10_nx91_v2") then
-	local ShipPos = {}
-	ShipPos[TEAM_Or] = Vector(-4720, 3883, 1208) 
-	ShipPos[TEAM_Bl] = Vector( 4720, 7973, 1208)
-	ShipPos[TEAM_Pu] = Vector(-4721.388672, -221.870987, 1208.031250)
-	ShipPos[TEAM_Gr] = Vector(-4731.745117, -4325.389160, 1208.031250)
-	else
-	local ShipPos = {}
-	ShipPos[TEAM_Or] = Vector(-4720, 3883, 1208) 
-	ShipPos[TEAM_Bl] = Vector( 4720, 7973, 1208)
+	ShipPos = {}
+	function GetMapSetPos()
+		if game.GetMap() == ("finalfrontier_a10_nx91_v2") then
+			ShipPos[TEAM_Or] = Vector(-4720, 3883, 1208)
+			ShipPos[TEAM_Bl] = Vector(-4720, 7982, 1208)
+			ShipPos[TEAM_Pu] = Vector(-4720, -222, 1208)
+			ShipPos[TEAM_Gr] = Vector(-4732, -4325, 1208)
+		else
+			ShipPos[TEAM_Or] = Vector(-4720, 3883, 1208)
+			ShipPos[TEAM_Bl] = Vector( 4720, 7973, 1208)
+		end
 	end
-end
-GetMapSetPos()
+	GetMapSetPos()
 end
 
 function CheckTeams()
-if team.NumPlayers(1)*4<team.NumPlayers(2)+team.NumPlayers(3)+team.NumPlayers(4) then
-return 1
-elseif team.NumPlayers(2)*4<team.NumPlayers(1)+team.NumPlayers(3)+team.NumPlayers(4) then
-return 2
-elseif team.NumPlayers(3)*4<team.NumPlayers(2)+team.NumPlayers(1)+team.NumPlayers(4) then
-return 3
-else
-return 4
-end
+	if team.NumPlayers(1)*4<team.NumPlayers(2)+team.NumPlayers(3)+team.NumPlayers(4) then
+		return 1
+	elseif team.NumPlayers(2)*4<team.NumPlayers(1)+team.NumPlayers(3)+team.NumPlayers(4) then
+		return 2
+	elseif team.NumPlayers(3)*4<team.NumPlayers(2)+team.NumPlayers(1)+team.NumPlayers(4) then
+		return 3
+	else
+		return 4
+	end
 end
 
 
 if SERVER then
-function ShipSet(ply)
-	if ply:Team() == TEAM_Or then
-		ply:SetPos(Vector(-4720, 3883, 1208))
-		end
-	if ply:Team() == TEAM_Bl then
-		ply:SetPos(Vector(4720, 7973, 1208))
-		end
-	if ply:Team() == TEAM_Pu then
-		ply:SetPos(Vector(-4721.388672, -221.870987, 1208.031250))
-		end
-	if ply:Team() == TEAM_Gr then
-		ply:SetPos(Vector(-4731.745117, -4325.389160, 1208.031250))
+	function ShipSet(ply)
+		if ply:Team() == TEAM_Or then
+			ply:SetPos(ShipPos[Team_OR])
+		elseif ply:Team() == TEAM_Bl then
+			ply:SetPos(ShipPos[Team_Bl])
+		elseif ply:Team() == TEAM_Pu then
+			ply:SetPos(ShipPos[Team_Pu])
+		elseif ply:Team() == TEAM_Gr then
+			ply:SetPos(ShipPos[Team_Gr])
 		end
 	end
+	function ShuffleTeams()
+		for k, v in pairs(player:GetAll()) do
+			v:SetTeam(10)
+			--v:ChatPrint("SpecTeam + "..team.GetName(v:Team()))
+		end
+		local TeamShuffle = TableShuffle(player:GetAll())
+		for k, v in pairs(TeamShuffle) do
+			v:SetTeam(CheckTeams())
+			--v:ChatPrint(team.GetName(v:Team()))
+		end
+		TeamShuffle = {}
 	end
-
-if SERVER then
-function ShuffleTeams()
-	for k, v in pairs(player:GetAll()) do
-		v:SetTeam(10)
-		--v:ChatPrint("SpecTeam + "..team.GetName(v:Team()))
-	end
-	local TeamShuffle = TableShuffle(player:GetAll())
-	for k, v in pairs(TeamShuffle) do
-		v:SetTeam(CheckTeams())
-		--v:ChatPrint(team.GetName(v:Team()))
-	end
-	TeamShuffle = {}
-end
 end
